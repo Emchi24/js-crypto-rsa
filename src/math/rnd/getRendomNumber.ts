Emchi24/js-crypto-rsa

commit 19e23bc2d09bd343059872c8a91d05b634a4849b

fix: improved browser support

src/math/rnd/getRendomNumber.ts
@@ -3,7 +3,8 @@ import { randomBytes } from 'crypto'
 export default function getRendomNumber(NumLenBits: number, oddNumber: boolean): bigint {
 
     // generate as many cryptografic random numbers as you want the bigger random number to be long
-    const rNumbArray = randomBytes(NumLenBits)
+    const rNumbArray = new Uint8Array(Math.ceil(NumLenBits / 8))
+    globalThis.crypto.getRandomValues(rNumbArray)
     
     let bitString: string = ""
     

src/generateKeyPair.ts
@@ -6,7 +6,7 @@ import extendedEuclideanAlgorithm from "./math/extendedEuclideanAlgorithm";
 export function getPrivateKey(p: bigint, q: bigint, bitLength: number) {
     const x = (p-1n) * (q-1n)
     while (true) {
-        const d = getRendomNumber((bitLength), false)
+        const d = getRendomNumber(bitLength, false)
         const relativePrimeCheck = gcd(d, x) === 1n
         if (relativePrimeCheck) {
             return d

src/hash.ts
@@ -4,7 +4,7 @@ export default async function createSha256Hash(input: string): Promise<string> {
   const encodedData = new TextEncoder().encode(input)
 
   // hash encoded data
-  const buffer = await crypto.subtle.digest('SHA-256', encodedData)
+  const buffer = await globalThis.crypto.subtle.digest('SHA-256', encodedData)
 
   // convert buffer to array
   const byteArray = Array.from(new Uint8Array(buffer))

src/index.ts
@@ -6,8 +6,6 @@ import decrypt from "./decrypt"
 import createSha256Hash from "./hash"
 import stringToObject from "./convertStringToObject"
 import objectToString from "./convertObjectToString"
-import { publicDecrypt } from "crypto"
-import { StringifyOptions } from "querystring"
 
 function isPublicKey(obj: any) {
     if (typeof obj != "object") {

commit 1e5a5cc880f09dffa6b6a0747bfdd6730b2d72d9

fix: bug where rNumbArray was the wrong size (introduced in 19e23bc)

src/math/rnd/getRendomNumber.ts
@@ -3,7 +3,7 @@ import { randomBytes } from 'crypto'
 export default function getRendomNumber(NumLenBits: number, oddNumber: boolean): bigint {
 
     // generate as many cryptografic random numbers as you want the bigger random number to be long
-    const rNumbArray = new Uint8Array(Math.ceil(NumLenBits / 8))
+    const rNumbArray = new Uint8Array(NumLenBits)
     globalThis.crypto.getRandomValues(rNumbArray)
     
     let bitString: string = ""